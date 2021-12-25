b2da8b572cddeec1a0981634be1fdb464fba9337
weizhonghuang <92793406+weizhonghuang@users.noreply.github.com>
2021-12-25 21:16:51 +0800

    Add files via upload

---
+++ Notepad.java
@@ -0,0 +1,10 @@
+public class Notepad {
+    int id;
+    String Notepad_Content;
+    String Notepad_Time;
+
+    @Override
+    public String toString() {
+        return "Notepad [id=" + id + ", Notepad_Content=" + Notepad_Content + ", Notepad_Time=" + Notepad_Time + "]";
+    }
+}
